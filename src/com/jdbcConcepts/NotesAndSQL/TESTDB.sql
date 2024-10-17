1ba1e3fab9e2527de68dad1724d6c25bd3989f38
karanAtreya1986 <karan1988@gmail.com>
2024-10-17 18:13:56 +0200

    added notes for jdbc lectures

---
+++ src/com/jdbcConcepts/NotesAndSQL/TESTDB.sql
@@ -0,0 +1,5 @@
+--create table employee(ID varchar(200),FIRSTNAME varchar(200), LASTNAME varchar(200), DESIGNATION varchar(200))
+
+select * from employee
+
+--insert into employee values(333, 'Rocky', 'Balboa', 'Boxer');
---
+++ src/com/jdbcConcepts/NotesAndSQL/test1.sql
@@ -0,0 +1,44 @@
+--create table employee(ID varchar(200),FIRSTNAME varchar(200), LASTNAME varchar(200), DESIGNATION varchar(200))
+
+--select * from employee
+
+--select * from books
+
+--insert into employee values(333, 'Rocky', 'Balboa', 'Boxer');
+--insert into employee values(111, 'Mcmahon', 'Rikishi', 'Cricket');
+--insert into employee values(222, 'Test', 'Scotty', 'Wrestler');
+
+--CREATE TABLE books (
+--    id NUMBER PRIMARY KEY,
+--    title VARCHAR2(255),
+--    author VARCHAR2(255),
+--    price DECIMAL(10, 2),
+--    quantity INTEGER
+--);
+
+--INSERT INTO books (id, title, author, price, quantity) VALUES (1, 'The Great Gatsby', 'F. Scott Fitzgerald', 10.99, 50);
+--INSERT INTO books (id, title, author, price, quantity) VALUES (2, '1984', 'George Orwell', 8.99, 35);
+--INSERT INTO books (id, title, author, price, quantity) VALUES (3, 'To Kill a Mockingbird', NULL, 7.49, 40);  -- Author is NULL
+--INSERT INTO books (id, title, author, price, quantity) VALUES (4, 'Pride and Prejudice', 'Jane Austen', 6.99, 20);
+--INSERT INTO books (id, title, author, price, quantity) VALUES (5, 'Moby-Dick', 'Herman Melville', 12.99, 15);
+
+--select * from user
+
+--CREATE TABLE emp_table (empid VARCHAR(3), name VARCHAR(30), PRIMARY KEY (empid));
+--CREATE TABLE emp_addr_table (empid VARCHAR(3), addrid VARCHAR(1), addr VARCHAR(30), city VARCHAR(20), state 
+--VARCHAR(20), PRIMARY KEY (empid, addrid ));
+
+--INSERT INTO emp_table VALUES ('1', 'William');
+--INSERT INTO emp_table VALUES ('2', 'Valentina');
+--INSERT INTO emp_addr_table VALUES ('1', '1', '41A Van Buren Street', 'Phoenix', 'Arizona');
+--INSERT INTO emp_addr_table VALUES ('1', '2', '100 University Drive', 'Sacramento', 'California');
+--INSERT INTO emp_addr_table VALUES ('2', '1', '12-E Nassau Street', 'New York', 'New York');
+
+--select * from emp_table
+--
+--select * from emp_addr_table
+
+--select * from books
+
+
+select * from survey
